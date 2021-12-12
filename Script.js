5f9459d0ff8ebb6a4c9d029fe6f08ea1b472a53a
YAZHINI1625 <95370212+YAZHINI1625@users.noreply.github.com>
2021-12-12 20:32:33 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,14 @@
+var request=new XMLHttpRequest();
+request.open('Get','https://jsonplaceholder.typicode.com/posts');
+request.send();
+request.onload=function(){
+   
+
+var data = JSON.parse(this.response);
+
+
+for(let i=0;i<data.length;i++){
+    console.log(data[i].id);
+}
+
+}
